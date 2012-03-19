6666393638ccf333df6e9dc7fc29a9f1523236c7
Larry Beall <lawrence.beall@gmail.com>
2012-03-19 01:24:23 -0400

    ...

--- Jabbr.WPF/Jabbr.WPF/Rooms/RoomViewModel.cs
+++ Jabbr.WPF/Jabbr.WPF/Rooms/RoomViewModel.cs
@@ -5,6 +5,9 @@ using System.Text;
 using Caliburn.Micro;
 using Jabbr.WPF.Infrastructure;
 using Jabbr.WPF.Messages;
+using Jabbr.WPF.Users;
+using System.ComponentModel;
+using Jabbr.WPF.Infrastructure.Models;
 
 namespace Jabbr.WPF.Rooms
 {
@@ -13,6 +16,11 @@ namespace Jabbr.WPF.Rooms
         private readonly JabbrManager _jabbrManager;
         private readonly ServiceLocator _serviceLocator;
         private readonly IObservableCollection<ChatMessageViewModel> _messages;
+        private readonly IObservableCollection<UserViewModel> _users;
+        private readonly AutoRefreshCollectionViewSource _ownerUsersSource;
+        private readonly AutoRefreshCollectionViewSource _activeUsersSource;
+        private readonly AutoRefreshCollectionViewSource _awayUsersSource;
+        private IEnumerable<string> _owners; 
 
         private bool _isPrivate;
         private int _userCount;
@@ -24,6 +32,14 @@ namespace Jabbr.WPF.Rooms
             _jabbrManager = jabbrManager;
             _serviceLocator = serviceLocator;
             _messages = new BindableCollection<ChatMessageViewModel>();
+            _users = new BindableCollection<UserViewModel>();
+            _ownerUsersSource = new AutoRefreshCollectionViewSource {Source = _users};
+            _activeUsersSource = new AutoRefreshCollectionViewSource {Source = _users};
+            _awayUsersSource = new AutoRefreshCollectionViewSource {Source = _users};
+
+            Owners.Filter = FilterOwnerUsers;
+            ActiveUsers.Filter = FilterActiveUsers;
+            AwayUsers.Filter = FilterAwayUsers;
         }
 
         public int UserCount
@@ -96,6 +112,21 @@ namespace Jabbr.WPF.Rooms
             get { return _messages; }
         }
 
+        public ICollectionView Owners
+        {
+            get { return _ownerUsersSource.View; }
+        }
+
+        public ICollectionView ActiveUsers
+        {
+            get { return _activeUsersSource.View; }
+        }
+
+        public ICollectionView AwayUsers
+        {
+            get { return _awayUsersSource.View; }
+        }
+
         internal void Initialize(RoomDetailsEventArgs roomDetailsEventArgs)
         {
             _jabbrManager.MessageReceived += JabbrManagerOnMessageReceived;
@@ -108,11 +139,33 @@ namespace Jabbr.WPF.Rooms
             UserCount = roomDetails.Users.Count();
             IsPrivate = roomDetails.Private;
             Topic = roomDetails.Topic;
+            _owners = roomDetails.Owners;
+
+            foreach (var user in roomDetails.Users)
+            {
+                AddUser(user);
+            }
+        }
+
+        private void AddUser(User user)
+        {
+            var userVm = _serviceLocator.GetViewModel<UserViewModel>();
+            bool isOwner = _owners.Any(x => x.Equals(user.Name));
+            userVm.Initialize(user, isOwner);
+
+            _users.Add(userVm);
         }
 
         private void JabbrManagerOnUserJoinedRoom(object sender, UserRoomSpecificEventArgs userRoomSpecificEventArgs)
         {
-            
+            if(!userRoomSpecificEventArgs.Room.Equals(RoomName, StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+            string userName = userRoomSpecificEventArgs.User.Name;
+            if(_users.Any(x => x.Name.Equals(userName, StringComparison.InvariantCultureIgnoreCase)))
+                return;
+
+            AddUser(userRoomSpecificEventArgs.User);
         }
 
         private void JabbrManagerOnRoomTopicChanged(object sender, RoomDetailsEventArgs roomDetailsEventArgs)
@@ -138,6 +191,33 @@ namespace Jabbr.WPF.Rooms
             Messages.Add(msgVm);
 
             UnreadMessageCount = Messages.Count(msg => !msg.Seen);
+        }
+
+        private bool FilterOwnerUsers(object data)
+        {
+            UserViewModel userVm = data as UserViewModel;
+            if (userVm == null)
+                return false;
+
+            return userVm.IsOwner;
+        }
+
+        private bool FilterAwayUsers(object data)
+        {
+            UserViewModel userVm = data as UserViewModel;
+            if (userVm == null)
+                return false;
+
+            return !userVm.IsOwner && userVm.IsAway;
+        }
+
+        private bool FilterActiveUsers(object data)
+        {
+            UserViewModel userVm = data as UserViewModel;
+            if (userVm == null)
+                return false;
+
+            return !userVm.IsOwner && !userVm.IsAway;
         }
     }
 }
