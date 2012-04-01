8781cd47a3e56931f4f3fe75a0ef6f231040149e
Larry Beall <lawrence.beall@gmail.com>
2012-04-01 02:22:07 -0400

    images and message processor

---
+++ Jabbr.WPF/Jabbr.WPF/Infrastructure/Services/MessagesProcessingService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jabbr.WPF.Infrastructure.Models;
+using Jabbr.WPF.Messages;
+using System.Threading;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using HtmlToXamlConversion;
+using System.Threading.Tasks;
+using Jabbr.WPF.Users;
+using JabbrModels = JabbR.Client.Models;
+
+namespace Jabbr.WPF.Infrastructure.Services
+{
+    public class MessageProcessingService
+    {
+        private readonly SynchronizationContext _uiContext;
+        private readonly ServiceLocator _serviceLocator;
+
+        public MessageProcessingService(ServiceLocator serviceLocator)
+        {
+            _uiContext = SynchronizationContext.Current;
+            _serviceLocator = serviceLocator;
+        }
+
+        public event EventHandler<MessageProcessedEventArgs> MessageProcessed;
+
+        public void ProcessMessageAsync(string room, JabbrModels.Message message)
+        {
+            Message msg = new Message(message);
+            ProcessMessageAsync(room, msg);
+        }
+
+        public void ProcessMessageAsync(string room, Message message)
+        {
+            InternalProcessMessage(room, message);
+        }
+
+        public ChatMessageViewModel ProcessMessage(JabbrModels.Message message)
+        {
+            Message msg = new Message(message);
+            return ProcessMessage(msg);
+        }
+
+        public ChatMessageViewModel ProcessMessage(Message message)
+        {
+            var task = InternalProcessMessage(null, message, true);
+            task.Wait();
+
+            return task.Result.MessageViewModel;
+        }
+
+        private Task<MessageProcessedEventArgs> InternalProcessMessage(string room, Message message, bool processSynchronously = false)
+        {
+            Task<MessageProcessedEventArgs> task = new Task<MessageProcessedEventArgs>(() =>
+            {
+                string content = ProcessEmoji(message.Content);
+                var inlines = ParseMessage(content);
+                var msgVm = _serviceLocator.GetViewModel<ChatMessageViewModel>();
+                var usrVm = _serviceLocator.GetViewModel<UserViewModel>();
+
+                usrVm.Initialize(message.User, false);
+
+                msgVm.Content = inlines;
+                msgVm.RawContent = message.Content;
+                msgVm.MessageDateTime = message.When.LocalDateTime;
+                msgVm.MessageId = message.Id;
+                msgVm.User = usrVm;
+
+                return new MessageProcessedEventArgs(msgVm, room);
+            });
+
+            if (!processSynchronously)
+            {
+                task.ContinueWith((completedTask) => OnMessageProcessed(completedTask.Result, _uiContext),
+                                  TaskContinuationOptions.OnlyOnRanToCompletion);
+                task.ContinueWith((errorTask) =>
+                {
+                    AggregateException exception = errorTask.Exception;
+
+                    if (exception == null)
+                        return;
+
+                    foreach (var innerException in exception.InnerExceptions)
+                    {
+                        System.Diagnostics.Trace.WriteLine(innerException.Message);
+                    }
+                }, TaskContinuationOptions.OnlyOnFaulted);
+            }
+
+            task.Start();
+
+            return task;
+        }
+
+        private static string ProcessEmoji(string content)
+        {
+            return content;
+        }
+
+        private static InlineCollection ParseMessage(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return null;
+
+            string xamlString = HtmlToXamlConverter.ConvertHtmlToXaml(content, false);
+            var parsedXaml = XamlReader.Parse(xamlString);
+            var xamlLines = ((Paragraph)((Section)parsedXaml).Blocks.FirstBlock).Inlines;
+
+            return xamlLines;
+        }
+
+        private void OnMessageProcessed(MessageProcessedEventArgs args, SynchronizationContext context = null)
+        {
+            if (context != null)
+            {
+                context.Post(d => OnMessageProcessed(args), null);
+                return;
+            }
+
+            var handler = MessageProcessed;
+            if (handler != null)
+                handler(this, args);
+        }
+    }
+}
--- Jabbr.WPF/Jabbr.WPF/Messages/ChatMessageViewModel.cs
+++ Jabbr.WPF/Jabbr.WPF/Messages/ChatMessageViewModel.cs
@@ -4,31 +4,85 @@ using System.Linq;
 using System.Text;
 using Jabbr.WPF.Infrastructure.Models;
 using Jabbr.WPF.Infrastructure;
+using Caliburn.Micro;
+using Jabbr.WPF.Users;
+using System.Windows.Documents;
 
 namespace Jabbr.WPF.Messages
 {
-    public class ChatMessageViewModel : MessageViewModel
+    public class ChatMessageViewModel : PropertyChangedBase
     {
-        private bool _seen;
+        private InlineCollection _content;
+        private DateTime _messageDateTime;
+        private string _messageId;
+        private UserViewModel _user;
+        private bool _hasBeenSeen;
 
-        public bool Seen
+        public string RawContent { get; set; }
+
+        public InlineCollection Content
         {
-            get { return _seen; }
+            get { return _content; }
             set
             {
-                if(_seen == value)
+                if(_content == value)
                     return;
 
-                _seen = value;
-                NotifyOfPropertyChange(() => Seen);
+                _content = value;
+                NotifyOfPropertyChange(() => Content);
             }
         }
 
-        internal void Initialize(Message message, bool isRoomVisible)
+        public DateTime MessageDateTime
         {
-            Content = message.Content;
-            Seen = isRoomVisible;
-            MessageTime = message.When.LocalDateTime;
+            get { return _messageDateTime; }
+            set
+            {
+                if(_messageDateTime == value)
+                    return;
+
+                _messageDateTime = value;
+                NotifyOfPropertyChange(() => MessageDateTime);
+            }
+        }
+
+        public string MessageId
+        {
+            get { return _messageId; }
+            set
+            {
+                if(_messageId == value)
+                    return;
+
+                _messageId = value;
+                NotifyOfPropertyChange(() => MessageId);
+            }
+        }
+
+        public UserViewModel User
+        {
+            get { return _user; }
+            set
+            {
+                if(_user == value)
+                    return;
+
+                _user = value;
+                NotifyOfPropertyChange(() => User);
+            }
+        }
+
+        public bool HasBeenSeen
+        {
+            get { return _hasBeenSeen; }
+            set
+            {
+                if(_hasBeenSeen == value)
+                    return;
+
+                _hasBeenSeen = value;
+                NotifyOfPropertyChange(() => HasBeenSeen);
+            }
         }
     }
 }
